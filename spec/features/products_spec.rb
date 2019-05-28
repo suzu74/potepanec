806b7e13995633dca7a0791ab91aafdcb5e9e528
Tomo <tomosuzu74@gmail.com>
2019-05-28 11:30:29 +0900

    Modify products_spec scenario name

--- spec/features/products_spec.rb
+++ spec/features/products_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.feature 'Products', type: :feature do
   let(:product) { create(:product) }
 
-  scenario 'ユーザーが商品詳細ページへ訪れる' do
+  scenario 'ユーザーが商品詳細ページへ訪れて、商品情報を見る' do
     visit potepan_product_path(product.id)
     expect(page).to have_content product.name
     expect(page).to have_content product.display_price
