fb16273b330890891dac73e012e65aa0519871c0
Tomo <tomosuzu74@gmail.com>
2019-06-06 15:41:07 +0900

    Create categories_controller_spec test

--- spec/controllers/potepan/categories_controller_spec.rb
+++ spec/controllers/potepan/categories_controller_spec.rb
@@ -2,8 +2,33 @@ require 'rails_helper'
 
 RSpec.describe Potepan::CategoriesController, type: :controller do
   describe "GET #show" do
+    
+    let(:taxon)    { create(:taxon, taxonomy: taxonomy) }
+    let(:taxonomy) { create(:taxonomy) }
+    let(:product)  { create(:product, taxons: [taxon]) }
+
+    before do
+      get :show, params: {id: taxon.id }
+    end
+
     it "レスポンスに成功" do
       expect(response).to be_successful
+    end
+
+    it "showテンプレートが表示されているか" do
+      expect(response).to render_template :show
+    end
+
+    it "正しい@taxonが渡されているか" do
+      expect(assigns(:taxon)).to eq(taxon)
+    end
+
+    it "正しい@taxonomyが渡されているか" do
+      expect(assigns(:taxonomies)).to match_array(taxonomy)
+    end
+
+    it "正しい@taxonが渡されているか" do
+      expect(assigns(:products)).to match_array(product)
     end
   end
 end
