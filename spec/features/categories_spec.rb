fc232b24e2af34a8ada8a5fa02076af1c3647e29
Tomo <tomosuzu74@gmail.com>
2019-06-10 09:58:20 +0900

    Modify features/categories test to be more simple

--- spec/features/categories_spec.rb
+++ spec/features/categories_spec.rb
@@ -11,14 +11,16 @@ RSpec.feature 'Categories', type: :feature do
   end
 
   scenario 'カテゴリーページに期待する情報が表示されている' do
-    expect(page).to have_content taxon.name
-    expect(page).to have_content taxonomy.name
-    expect(page).to have_content "(#{taxon.all_products.count})"
-  end
+    within '.side-nav' do
+      expect(page).to have_content taxon.name
+      expect(page).to have_content taxonomy.name
+      expect(page).to have_content "(#{taxon.all_products.count})"
+    end
 
-  scenario 'カテゴリーページに商品の情報が表示される' do
-    expect(page).to have_content product.name
-    expect(page).to have_content product.display_price
+    within '.productBox' do
+      expect(page).to have_content product.name
+      expect(page).to have_content product.display_price
+    end
   end
 
   scenario 'カテゴリーページから商品詳細ページへ飛び、商品名が表示されている' do
