152c17cf9609219040f08b17c7264508e686d189
Tomo <tomosuzu74@gmail.com>
2019-06-10 09:15:20 +0900

    Solve N+1 problem to images

--- app/controllers/potepan/categories_controller.rb
+++ app/controllers/potepan/categories_controller.rb
@@ -2,6 +2,6 @@ class Potepan::CategoriesController < ApplicationController
   def show
     @taxon = Spree::Taxon.find(params[:id])
     @taxonomies = Spree::Taxonomy.all.includes(:root)
-    @products = @taxon.all_products.includes(master: [:default_price])
+    @products = @taxon.all_products.includes(master: [:default_price, :images])
   end
 end
