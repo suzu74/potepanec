3b0e057881d5efab8502270e21176cb6d75ef361
Tomo <tomosuzu74@gmail.com>
2019-06-12 19:40:24 +0900

    Create related_product section

--- app/controllers/potepan/products_controller.rb
+++ app/controllers/potepan/products_controller.rb
@@ -1,5 +1,6 @@
 class Potepan::ProductsController < ApplicationController
   def show
     @product = Spree::Product.find(params[:id])
+    @related_products = Spree::Product.related_products(@product).limit(4).shuffle
   end
 end
