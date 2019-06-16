2ba3fab707c48e4952a1a6843f62311f62f46545
Tomo <tomosuzu74@gmail.com>
2019-06-16 09:57:32 +0900

    Avoid magic_number at products_controller

--- app/controllers/potepan/products_controller.rb
+++ app/controllers/potepan/products_controller.rb
@@ -1,6 +1,8 @@
 class Potepan::ProductsController < ApplicationController
+  MAX_NUMBER_OF_RELATED_PRODUCTS = 4
+
   def show
     @product = Spree::Product.find(params[:id])
-    @related_products = @product.related_products.includes(master: [:default_price, :images]).shuffle.take(4)
+    @related_products = @product.related_products.includes(master: [:default_price, :images]).shuffle.take(MAX_NUMBER_OF_RELATED_PRODUCTS)
   end
 end
